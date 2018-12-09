ac2fe1f52d51afa003a62478d85a9bb5cd97af68
uhmtoto <uhmtotow@gmail.com>
2018-12-09 17:25:57 +0900

    1015~1016

---
+++ 1015.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    int a = 55, b = 10;
+    printf("%d - %d = %d\n", a, b, a-b);
+    a = 2008, b = 1999;
+    printf("%d - %d = %d\n", a, b, a-b);
+}
---
+++ 1016.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    int w = 49;
+    double g = 0.2683;
+    printf("%d * %lf = %lf\n", w, g, w*g);
+    return 0;
+}
